bcaf5324f9e83e6b91acf276059cbd3e5ab7307f
elidio123 <elidio.laureano@fatec.sp.gov.br>
2021-08-27 14:57:12 +0000

    Semana 4 Estrutura Decisao Repeticao

---
+++ ExerFor4.java
@@ -0,0 +1,15 @@
+public class ExerFor4{
+    //Imprima os fatoriais de 1 a 10.
+    public void fatorial(){
+
+        //5*4*3*2*1
+        int fat=1;
+        for(int n=5;n>=1;n=n-1){
+            fat=fat*n;
+            
+        }
+        System.out.println(fat);
+
+    }
+
+}
---
+++ ExerFor5.java
@@ -0,0 +1,21 @@
+public class ExerFor5{
+    public void par_impar(){
+
+        int x=58;
+        if(x%2==0){
+            x = x / 2;
+            System.out.println(x);
+        }
+        else {
+            x=3*x+1;
+            System.out.println(x);
+        }   
+
+    }
+}
+
+/*Escreva um programa que, dada uma variável x com algum valor inteiro, temos um
+    //novo x de acordo com a seguinte regra:
+    //se x é par, x = x / 2
+    //se x é impar, x = 3 * x + 1
+    //imprime x    */ 
